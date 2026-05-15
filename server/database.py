639027c24d69daf6724c86328c9d3435fe706fc2
kinokist <kinokist@gmail.com>
2026-05-15 23:26:34 +0900

    set6

--- server/database.py
+++ server/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://juvi:Wd03Ylkpn5ng1rGCMpV37jBPEvp58JO3@localhost:5432/mydb"
+DATABASE_URL = "postgresql://juvi:Wd03Ylkpn5ng1rGCMpV37jBPEvp58JO3@dpg-d82o49f7f7vs738bnggg-a.singapore-postgres.render.com/juvi"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
