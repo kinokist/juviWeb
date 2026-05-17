95f5dd909c8067afa6f46210a0061fd71de39fa1
kinokist <kinokist@gmail.com>
2026-05-17 09:21:59 +0900

    py1

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from server.database import SessionLocal, engine
 from server import models, crud, schemas
@@ -6,6 +7,19 @@ from server import models, crud, schemas
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "https://kinokist.github.io",   # 프론트엔드 도메인
+    "http://localhost:8000"         # 로컬 테스트용
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
